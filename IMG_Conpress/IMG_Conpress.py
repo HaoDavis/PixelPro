40731af5a0206ac3d4f1ce75725c2ab3035846f6
e1ecb0t <1003544078@qq.com>
2023-11-20 11:47:03 +0800

    1.0

--- IMG_Conpress/IMG_Conpress.py
+++ IMG_Conpress/IMG_Conpress.py
@@ -1,43 +1,50 @@
 import os
 import logging
+import tempfile
 
 from PIL import Image
 
+logging.basicConfig(filename='compress.log',level=logging.DEBUG)
 
 MIN_QUALITY = 20
 
 def get_size(filename):
     return os.path.getsize(filename)
 
-def conpress_image(file_in, file_out='',mb=1500):
+def conpress_image(file_in, file_out=None,target_size=1500):
     """
     compress image without changing any img_size
     :param file_in: 源文件
     :param file_out:  out地址
-    :param mb: 大小
-    :param step:  调整比率
-    :param equlity: 初始压缩比率
+    :param target_size: 大小 单位KB
     :return:  压缩文件地址
     """
     if not os.path.exists(file_in):
         raise FileNotFoundError
 
-    Dir, fmt = file_in.split('.')
-    file_out = Dir + '_out.' + fmt if file_out == '' else file_out
 
-    quality = 80
-    while True:
-        img = Image.open(file_in)
-        img.save(file_out,quality=quality)
+    basename,ext = os.path.splitext(os.path.basename(file_in))
+    file_out = file_out or f'{basename}_compressed{ext}'
 
-        size = get_size(file_out)
-        a_size = size/1024
-        if a_size<=mb or quality < MIN_QUALITY:
-            break
+    logging.info(f'Compressing {file_in} to {file_out}')
 
-        quality -=10
+    try:
+        quality = 50
+        while True:
+            with Image.open(file_in) as img:
+                img.save(file_out,quality=quality)
 
-    return file_out,size
+            size = get_size(file_out)
+            logging.info(f'Quality: {quality}, Size:{size/1024}KB')
+
+            if size/1024 <=target_size or quality < MIN_QUALITY:
+                break
+
+            quality -=10
+    except Exception as e:
+        logging.error(e)
+
+    return file_out,get_size(file_out)
 
 if __name__ == '__main__':
-    conpress_image('computer_kuan_201632_18.jpg')
+    conpress_image('computer_kuan_201632_18.jpg',target_size=1024)
